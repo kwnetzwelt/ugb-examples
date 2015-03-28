5ddb9841707b87689ab9a2642f767d146b6fd5a9
Kai Wegner <kwnetzwelt@gmail.com>
2015-03-28 09:09:11 +0100

    Added Component Example

--- GameLogic/GameComponentExample.cs
+++ GameLogic/GameComponentExample.cs
@@ -1,15 +1,46 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityGameBase;
 
-public class GameComponentExample : MonoBehaviour {
+public class GameComponentExample : UGBExampleBase {
 
-	// Use this for initialization
-	void Start () {
-	
+	MyGameComponentExample component;
+
+	void OnEnable()
+	{
+		component = this.gameObject.AddComponent<MyGameComponentExample>();
+		Debug.Log(component.Game.testing);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+
+	void OnDisable()
+	{
+		Destroy(component);
+		component = null;
 	}
+
+	#region implemented abstract members of UGBExampleBase
+	public override int Index {
+		get {
+			return 0;
+		}
+	}
+	public override string Title {
+		get {
+			return "GameComponent";
+		}
+	}
+	public override string Description {
+		get {
+			return "UGB provides a base class for your components to simplify access to your game implementation. " +
+				"Instead of deriving from MonoBehaviour your scripts Should derive from GameComponent. " +
+				"You provide the type of your game as a parameter to GameComponent. ";
+		}
+	}
+	#endregion
+
+}
+
+public class MyGameComponentExample : GameComponent<EmptyGame>
+{
+
 }
